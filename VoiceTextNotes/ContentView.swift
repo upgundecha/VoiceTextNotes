ed91f1312bece4af9aa7f32d5e1222b8512709b3
Unmesh Gundecha <upgundecha@gmail.com>
2024-01-21 14:59:50 +0800

    Initial Commit

---
+++ VoiceTextNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceTextNotes
+//
+//  Created by Unmesh Gundecha on 21/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceTextNotes/VoiceTextNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceTextNotesApp.swift
+//  VoiceTextNotes
+//
+//  Created by Unmesh Gundecha on 21/1/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceTextNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
